d9137418158c51585cff520140df81ee68d2b0b7
Nik <nikitaholub1999@gmail.com>
2019-06-02 05:59:07 +0300

    dfd

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
 export default ({markup, css}) => {
     return `<!doctype html>
-      <html lang="en">
+      <html lang="ru">
         <head>
           <meta charset="utf-8">
-          <title>MERN Marketplace</title>
+          <title>Альта</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
           <style>
